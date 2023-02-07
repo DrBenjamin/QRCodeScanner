2772137cef1832dc97e7cb7b0225d349c6810e84
Benjamin Gross <ben@benbox.org>
2023-02-07 11:54:02 +0200

    credentials download cached

--- QRCodeScanner.py
+++ QRCodeScanner.py
@@ -135,10 +135,10 @@ elif option == 'Identify':
   st.subheader('QR Code Generator')
   emp =  st.text_input(label = 'Please enter an employee number')
   pin =  st.text_input(label = 'Please enter the PIN')
-  if url != "":
-    if url[len(url) - 1] == '/':
-      url = url[:-1]
-    qrcode_image = generate_qrcode(url + '/?eno=' + emp + '&pin=' + pin)
+  if emp != '':
+    if emp[len(url) - 1] == '/':
+      emp = emp[:-1]
+    qrcode_image = generate_qrcode(emp + '/?eno=' + emp + '&pin=' + pin)
   else:
     qrcode_image = None
   if qrcode_image != None:
